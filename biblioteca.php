862923f08896fb4d115f6b9ab24f7a26cf2df341
João Vitor da Silva Correia <joaovitorsilvacorreia@gmail.com>
2024-06-19 00:02:52 -0300

    Add files via upload

---
+++ biblioteca.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Biblio-Tec</title>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" href="../imagens/Biblio-tec.png" type="image/x-icon" />
+		<link rel="stylesheet" type="text/css" href="css/css.css">
+		<link rel="stylesheet" type="text/css" href="css/csspagina.css">
+
+	</head>
+	<body>
+		<div id="container">
+			<?php
+			$var = "";
+			include_once("includes/cabecalho.php");
+			include_once("includes/menu.php");
+
+			?>
+			<div id="corpo">
+				
+				<h2>
+				<br> <br><br> <br><br> <br><br> <br><br> 
+				</h2>
+				
+			
+					<?php
+						include_once("includes/rodape.php");
+					?>
+	</body>
+</html>
